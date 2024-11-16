d8c04c2b212aac33b839cc29efc942c8a92110a2
paoyako24 <paolaparedes248@gmail.com>
2024-11-16 11:40:40 -0600

    Add files via upload

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { Character } from '../interfaces/character.interace';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -6,6 +7,17 @@ import { Component} from '@angular/core';
 })
 
 export class MainPageComponent  {
+
+  public characters: Character[]= [{
+    name:'Kril',
+    power: 1000
+  },{
+      name:'Goku',
+      power:9500
+  },{
+        name:'Vegeta',
+        power: 7500
+}];
 
 
 }
