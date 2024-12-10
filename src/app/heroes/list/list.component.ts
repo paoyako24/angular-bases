dd8f2d8e5b2be4f8bc99dc33cef334b5c0a25715
paoyako24 <paolaparedes248@gmail.com>
2024-12-09 18:10:31 -0600

    Add files via upload

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -1,18 +1,24 @@
-import { Component,Input } from '@angular/core';
-import { Character } from '../../dbz/interfaces/character.interace';
+import { Character } from './../../interfaces/character.interace';
+import { Component,Input } from "@angular/core";
 
 
 @Component({
-  selector: 'dbz-list',
-  templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
-})
-export class ListComponent {
+  selector:'dbz-list',
+  templateUrl:'./list.component.html',
+  styleUrls:['./list.component.css']
+ })
+ export class ListComponent {
 
-@Input()
- public characterList: Character[] = [{
-   name:'Trunks',
-   power:10
- }]
+ @Input()
+ public  characterList: Character[] = [{
+  name:'Trunks',
+  power:10
+ }];
 
-}
+ //onDelete = Index value : number
+
+ onDeleteCharacter( index: number) :void {
+//TODO:Emitir el ID del personaje
+ console.log(index)
+ }
+ }
